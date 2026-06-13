8e6c058e3d45bb35404f565bdd9262d2cd9c724c
Erneste IZERE NEZA <studizere@gmail.com>
2026-06-13 19:27:24 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const navigation = document.getElementById("navLinks");
+
+const showMenu = () => {
+    navigation.style.right = "0";
+}
+
+const hideMenu = () => {
+    navigation.style.right = "-200px";
+}
